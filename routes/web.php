c0aa3a885ab03e28ec332197c2294011a1253078
swimmerw <swimmerw@gmail.com>
2022-10-30 14:41:46 -0300

    Commit de prueba

--- routes/web.php
+++ routes/web.php
@@ -15,14 +15,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(['verify'=> true]);
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('test', function(){
+    return 'Hola desde un closure';
+});
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
